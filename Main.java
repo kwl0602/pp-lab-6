411c665a2e347be1a77ca94a4c6189ed22db2f84
unknown <daavwid2001@gmail.com>
2024-05-05 12:32:21 +0200

    zad3

--- Main.java
+++ Main.java
@@ -1,4 +1,3 @@
-
 import company.models.Manager;
 import company.models.Worker;
 import company.abstracts.Employee;
@@ -9,22 +8,24 @@ import java.util.List;
 public class Main {
     public static void main(String[] args) {
         Worker w1 = new Worker("Michał", 50000, 1, "2021-01-10", "Księgowy");
-        Worker w2 = new Worker("Aleksander", 52000, 2, "2020-03-15", "Inżynier");
+        Worker w2 = new Worker("Aleksander", 52000, 1, "2020-03-15", "Inżynier");
         Worker w3 = new Worker("Aleksandra", 54000, 3, "2022-05-20", "Informatyk");
-        Worker w4 = new Worker("Tyomoteusz", 56000, 4, "2023-02-09", "Teleinformatyk");
         Manager m1 = new Manager("Marcin", 100000, 5, "2019-08-01", "Dyrektor");
 
         List<Employee> employees = new ArrayList<>();
         employees.add(w1);
         employees.add(w2);
         employees.add(w3);
-        employees.add(w4);
         employees.add(m1);
 
         for (Employee emp : employees) {
-            emp.work();
-            System.out.printf("- %s (ID: %d, Stanowisko: %s, Data zatrudnienia: %s, Wynagrodzenie: $%.2f).\n",
-                    emp.getName(), emp.getId(), emp.getPosition(), emp.getHireDate(), emp.getSalary());
+            System.out.println(emp.getName() + " has code: " + emp.hashCode());
+        }
+
+        System.out.println("Comparing " + w2.getName() + " with other employees:");
+        for (Employee emp : employees) {
+            boolean isEqual = w2.equals(emp);
+            System.out.println("Comparing to " + emp.getName() + ": " + isEqual);
         }
     }
 }
--- company/abstracts/Employee.java
+++ company/abstracts/Employee.java
@@ -41,4 +41,13 @@ public abstract class Employee implements Employable {
     public int hashCode() {
         return id;
     }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj == null || getClass() != obj.getClass()) {
+            return false;
+        }
+        Employee other = (Employee) obj;
+        return id == other.id;
+    }
 }
